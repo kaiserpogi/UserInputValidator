58f00295ebb488e82a1e40700c40092563624459
AevronAndreiBarit <AevronAndreiBarit@SMU-0115>
2025-09-16 09:02:11 +0800

    Add project files.

---
+++ UserInputValidator/Form1.Designer.vb
@@ -0,0 +1,73 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.btnValidator = New System.Windows.Forms.Button()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtAge
+        '
+        Me.txtAge.Location = New System.Drawing.Point(143, 135)
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(100, 20)
+        Me.txtAge.TabIndex = 0
+        '
+        'btnValidator
+        '
+        Me.btnValidator.Location = New System.Drawing.Point(145, 185)
+        Me.btnValidator.Name = "btnValidator"
+        Me.btnValidator.Size = New System.Drawing.Size(75, 23)
+        Me.btnValidator.TabIndex = 1
+        Me.btnValidator.Text = "Validate"
+        Me.btnValidator.UseVisualStyleBackColor = True
+        '
+        'lblResult
+        '
+        Me.lblResult.AutoSize = True
+        Me.lblResult.Location = New System.Drawing.Point(145, 225)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(37, 13)
+        Me.lblResult.TabIndex = 2
+        Me.lblResult.Text = "Result"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.btnValidator)
+        Me.Controls.Add(Me.txtAge)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents btnValidator As Button
+    Friend WithEvents lblResult As Label
+End Class
---
+++ UserInputValidator/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+    Private Sub btnValidator_Click(sender As Object, e As EventArgs) Handles btnValidator.Click
+        Try
+            If IsNumeric(txtAge.Text) And txtAge.Text > 0 Then
+                lblResult.Text = "You are " + txtAge.Text + " years old"
+
+            End If
+
+            If txtAge.Text <= 0 Then
+                Throw New FormatException
+
+            End If
+        Catch ex As FormatException
+            lblResult.Text = "Age cannot be niggative."
+
+
+
+        End Try
+
+    End Sub
+End Class
